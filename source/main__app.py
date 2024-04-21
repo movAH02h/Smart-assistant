86cd1653e85716d0f4f17bc68526723602af93bf
Kirill Rumyantsev <KARum2004@yandex.ru>
2024-04-21 22:08:16 +0300

    improve data set and functions

---
+++ source/main__app.py
@@ -0,0 +1,82 @@
+import threading
+import tkinter as tk
+from source.functions import recognise
+import pickle
+from tensorflow import keras
+import torch
+import os
+
+class App(tk.Tk):
+    def __init__(self, model, tokenizer, lbl_encoder, speak_model):
+        super().__init__()
+        #Инициализация полей для ИИ
+        self.model = model
+        self.tokenizer = tokenizer
+        self.lbl_encoder = lbl_encoder
+        self.speak_model = speak_model
+
+        self.title("Голосовой помощник Галя")
+        self.geometry('600x500')
+        self.resizable(False, False)
+        self.configure(bg="pink")
+        self.button = tk.Button(self,
+                                command=self.start_action,
+                                width=20,
+                                height=5,
+                                activeforeground="#FFFFFF",
+                                activebackground="#22B14C",
+                                text="Говорить")
+        self.button.pack()
+        self.button.place(relx=0.5, rely=0.5, anchor='center')
+
+    def start_action(self):
+        self.button.config(state=tk.DISABLED)
+        self.button.config(text="Слушаю и выполняю")
+        #click - лямбда-функция, которая работает с помощью обуцченной модели
+        thread = threading.Thread(target=lambda: recognise(self.speak_model, self.model, self.tokenizer, self.lbl_encoder))
+        thread.start()
+        self.check_thread(thread)
+
+    def check_thread(self, thread):
+        if thread.is_alive():
+            # используется именно lambda функция, потому что нам надо передать функцию как аргумент.
+            # Т.к она должна вызваться после задержки
+            # если не использовать lambda, то функция уйдет в рекурсию и выпадет ошибка
+            # https://stacktuts.com/how-to-use-after-method-in-tkinter - сайт с объяснением
+            self.after(100, lambda: self.check_thread(thread))
+        else:
+            self.button.config(state=tk.NORMAL, text="Говорить")
+
+def main():
+    # load speaking model
+    device = torch.device('cpu')
+    torch.set_num_threads(4)
+    local_file = 'model.pt'
+
+    if not os.path.isfile(local_file):
+        torch.hub.download_url_to_file('https://models.silero.ai/models/tts/ru/v4_ru.pt',
+                                       local_file)
+
+    # Загрузка существующей модели - если есть на компьютере
+    speak_model = torch.package.PackageImporter(local_file).load_pickle("tts_models", "model")
+    speak_model.to(device)
+
+    # load trained model
+    model = keras.models.load_model('../neural_network/chat_model.keras')
+
+    # load tokenizer object
+    with open('../neural_network/tokenizer.pickle', 'rb') as handle:
+        tokenizer = pickle.load(handle)
+
+    # load label encoder object
+    with open('../neural_network/label_encoder.pickle', 'rb') as enc:
+        lbl_encoder = pickle.load(enc)
+
+    app = App(model, tokenizer, lbl_encoder, speak_model)
+    app.mainloop()
+
+if __name__ == "__main__":
+    main()
+
+
+
